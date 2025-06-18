7e16d2fd0dc9e822f388b2f3c4c3e178f44a6e39
Jose Cortes <j.c.e555@hotmail.com>
2025-06-18 16:49:38 -0500

    Ajuste js

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,14 +1,22 @@
+/*Mensaje de bienvenida*/
 console.log("Bienvenido al sistema de registro de Riwi");
 
+/*Declaracion de variables*/
 let nombre;
 let edad;
 
+/*Ciclo while para que cuando se ingrese un nombre no valido se repita*/
 while (true) {
+
+  /*Se pide al usuario el nombre*/
   nombre = prompt("Ingrese su nombre (solo letras)", "Ej: Ana Pérez");
 
+  /*Variable donde se valida solo letras y espacios*/
   const soloLetras = /^[a-zA-Z\s]+$/;
 
+  /*Validacion nombre solo con letras y espacios*/
   if (nombre && soloLetras.test(nombre)) {
+    console.log(`Nombre ${nombre} ingresado correctamente`);
     break;
   } else {
     alert("Error: El nombre debe contener solo letras y espacios. Intenta nuevamente.");
@@ -16,12 +24,22 @@ while (true) {
   }
 }
 
+/*Ciclo while para que cuando se ingrese una edad no valido se repita*/
 while (true) {
+
+  /*Se pide al usuario la edad*/
   edad = prompt("Ingrese su edad", "Solo números");
+
+  /*Se convierte edad a numero entero*/ 
   edad = parseInt(edad);
 
+
+  /*Validacion edad solo con nuneros*/
   if (!isNaN(edad)) {
 
+    console.log(`Edad ${edad} ingresada correctamente`);
+    
+    /*Validaciones de edad*/
     if (edad < 18) {
       console.log(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!`);
       alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!`);
